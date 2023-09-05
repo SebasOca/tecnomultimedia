05a5f6118420d3455bb05dc1e2e0425be0254654
SebasOca <129012466+SebasOca@users.noreply.github.com>
2023-09-05 15:19:57 -0300

    Add files via upload

---
+++ tp4/juego.js
@@ -0,0 +1,12 @@
+function atlantis(){
+  image(ciudad, 0, 0, 800, 600);
+  
+  
+}
+
+function enemigos(){
+    image(en1, 0, 80, 60, 25);
+    image(en2, 100, 140, 60, 25);
+    image(en3, 200, 200, 60, 25);  
+  
+}
---
+++ tp4/tp4.js
@@ -0,0 +1,27 @@
+//Enlace al video:
+let ciudad, en1, en2, en3, inicial, b1, b2, b3;
+let fuente; 
+
+function preload(){
+  ciudad = loadImage('data/fondo.png');
+  en1 = loadImage('data/enemigo1.png');
+  en2 = loadImage('data/enemigo2.png');
+  en3 = loadImage('data/enemigo3.png');
+  inicial = loadImage('data/inicial.png');
+  b1 = loadImage('data/botoninicial.png');
+  b2 = loadImage('data/botoncreditos.png');
+  b3 = loadImage('data/botonreiniciar.png');
+  fuente = loadFont(RetroGaming.vlw);
+}
+
+function setup() {
+  createCanvas(800, 600);
+  inicio();
+  
+
+}
+
+
+function draw() {
+ // enemigos();
+}
