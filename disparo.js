c1bc2ff0fc2bb22be9732fc7d1a16e06b399cb3f
SebasOca <129012466+SebasOca@users.noreply.github.com>
2023-10-21 19:31:44 -0300

    Add files via upload

--- disparo.js
+++ disparo.js
@@ -5,16 +5,16 @@ class Disparo {
     this.imagenD = rayoD;
     this.posXI = 38;
     this.posYI = 464;
-    //this.posXD1 = 715;
-    //this.posYD1 = 402;
+    this.posXD1 = 715;
+    this.posYD1 = 402;
   }
 
   dibujar() {  //9 en x, 7 en y
     if (keyCode == LEFT_ARROW) {
       image(this.imagenI, this.posXI++, this.posYI--, 27, 21);
     }
-    //if (keyCode == RIGHT_ARROW) {
-    //  image(this.imagenD, this.posXD1--, this.posYD1--, 27, 21);
-    //}
+    if (keyCode == RIGHT_ARROW) {
+      image(this.imagenD, this.posXD1--, this.posYD1--, 27, 21);
+    }
   }
 }
