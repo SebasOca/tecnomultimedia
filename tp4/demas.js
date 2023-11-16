9e1b1741dd4c7fc56dcf3eb33507c1770ead9e1f
SebasOca <129012466+SebasOca@users.noreply.github.com>
2023-11-16 17:57:28 -0300

    Add files via upload

---
+++ tp4/demas.js
@@ -0,0 +1,52 @@
+//function enemigos() {
+//  image(en2, posEX++, 140, 60, 25);
+//  image(en1, posEX2--, 100, 60, 25);
+//}
+
+//function cañones() {
+//  //izq
+//  for (let i=0; i<cantDisp; i++) {
+//    if (disparoI && disparoIS[i] == false) {
+//      disparoIS[i] = true;
+//      disparoI = false;
+//    }
+//  }
+//  for (let i=0; i<cantDisp; i++) {
+//    fill(221, 229, 12);
+//    ellipse(dispIPosX[i], dispIPosY[i], 10, 10);
+//    if (disparoIS[i] == true) {
+//      dispIPosX[i]+=14;
+//      dispIPosY[i]-=8;
+//    }
+//  }
+//  //der
+//  for (let i=0; i<cantDisp; i++) {
+//    if (disparoD && disparoDS[i] == false) {
+//      disparoDS[i] = true;
+//      disparoD = false;
+//    }
+//  }
+//  for (let i=0; i<cantDisp; i++) {
+//    fill(221, 229, 12);
+//    ellipse(dispDPosX[i], dispDPosY[i], 10, 10);
+//    if (disparoDS[i] == true) {
+//      dispDPosX[i]-=14;
+//      dispDPosY[i]-=8;
+//    }
+//  }
+//}
+
+//function puntosYTiempo() {
+//  fill(221, 229, 12);
+//  textFont(fuente);
+//  textSize (30);
+//  text (tiempo, 180, 575);
+//  text (mouseX, 100, 100);
+//  text (mouseY, 180, 100);
+
+//  tiempo = tiempoI - second();
+//  if (tiempo <= 0) {
+//    tiempo = tiempoI;
+//  }
+//  text ("Puntos: " + 0, 450, 575);
+//}
---
+++ tp4/pantallas.js
@@ -0,0 +1,66 @@
+function pantallas() {
+  textFont(fuente);
+  textAlign(CENTER);
+  fill(221, 229, 12);
+
+  if (pantalla == 0) {
+    //inicial
+    image(inicial, 0, 0, 800, 600);
+    image(b2, 150, 450, 100, 39);
+    image(b1, 325, 440, 150, 58);
+    image(b3, 550, 450, 100, 39);
+  } else if (pantalla == 1) {
+    //instrucciones
+    image(fondo, 0, 0, 800, 600);
+    image(b5, 660, 545, 100, 39);
+    image(torreI, 0, 354, 40, 23);
+    image(torreC, 360, 320, 40, 23);
+    image(torreD, 760, 320, 40, 23);
+    image(ciudad, 80, 487, 81, 22);
+    image(domo, 190, 421, 80, 25);
+    image(puente, 480, 485, 80, 24);
+    image(en2, 190, 140, 60, 25);
+    image(en1, 500, 100, 60, 25);
+
+
+    textSize(25);
+    text('Intenta sobrevivir un minuto frente a las naves enemigas', 400, 70);
+    textSize(18);
+    text('Pulsa "izquierda" para abrir fuego\ncon la torre izquierda,\ny "derecha" para la torre derecha', 170, 220);
+    text('Pulsa "arriba" para atacar con el canon central', 400, 310);
+    text('Elimina a los invasores antes\n de que destruyan la ciudad', 640, 220);
+    text('Cada enemigo abatido da 10 puntos', 380, 570);
+  } else if (pantalla == 2) {
+    //juego
+    image(fondo, 0, 0, 800, 600);
+    image(b4, 40, 545, 100, 39);
+    image(b5, 660, 545, 100, 39);
+
+    //defensas
+    cañones();
+    image(torreI, 0, 354, 40, 23);
+    image(torreC, 360, 320, 40, 23);
+    image(torreD, 760, 320, 40, 23);
+
+    //proteger
+    image(ciudad, 80, 487, 81, 22);
+    image(domo, 190, 421, 80, 25);
+    image(puente, 480, 485, 80, 24);
+
+    if (keyCode === 32) {
+      enemigos();
+      control();
+      //puntosYTiempo();
+    }
+  } else if (pantalla == 3) {
+    //creditos
+    image(fondocred, 0, 0, 800, 600);
+    image(b5, 660, 545, 100, 39);
+
+    textSize(40);
+    text('Creditos', 400, 200);
+    textSize(25);
+    text ('Recreado por Sebastian Ocampo', 400, 300 );
+    text('Basado en el juego de Imagic', 400, 360);
+  }
+}
