f783ad5ddbb64dc8a73cccd463e075d6c4dc016b
SebasOca <129012466+SebasOca@users.noreply.github.com>
2023-11-17 16:51:46 -0300

    Add files via upload

--- tp5/proyectil.js
+++ tp5/proyectil.js
@@ -7,14 +7,22 @@ class Proyectil {
     this.posYI = 464;
     this.posXD = 715;
     this.posYD = 402;
+    this.proyectilDisparado = false;
+    this.cantProyectiles = 50;
   }
 
   dibujar() {  //9 en x, 7 en y
-    if (keyCode == LEFT_ARROW) {
-      image(this.imagenI, this.posXI++, this.posYI--, 27, 21);
+    if (!this.proyectilDisparado) {
+      if (keyCode == LEFT_ARROW) {
+        image(this.imagenI, this.posXI++, this.posYI--, 27, 21);
+      }
+      if (keyCode == RIGHT_ARROW) {
+        image(this.imagenD, this.posXD--, this.posYD--, 27, 21);
+      }
     }
-    if (keyCode == RIGHT_ARROW) {
-      image(this.imagenD, this.posXD--, this.posYD--, 27, 21);
-    }
+  }
+
+  disparar() {
+    this.proyectilDisparado =true;
   }
 }
